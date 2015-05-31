556adc3c17f922a98762d4193b14dc75b15efa87
Ken Schmidt <kenschmidt@gmail.com>
2015-05-31 15:17:49 -0700

    adding event page and removing scheduler page

--- app/models/event.js
+++ app/models/event.js
@@ -1,10 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	track: DS.belongsTo('track'),
+	track: DS.belongsTo('track', {async: true}),
 	title: DS.attr('string'),
 	speaker: DS.attr('string'),
 	day: DS.attr('string'),
 	time: DS.attr('string'),
-	location: DS.belongsTo('location')
+	location: DS.belongsTo('location', {async: true})
 });
--- app/models/track.js
+++ app/models/track.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  style: DS.attr('string')
 });
