3af4a5784c8c3537d1b8682ae164a11f1621d39e
Ken Schmidt <kenschmidt@gmail.com>
2015-06-07 11:14:42 -0700

    I thought readOnly was working last night, I guess I am going to make it work myself

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,10 @@
 // app/serializers/application.js
 import FirebaseSerializer from 'emberfire/serializers/firebase';
 
-export default FirebaseSerializer.extend();
+export default FirebaseSerializer.extend({
+  serializeAttribute(snapshot, json, key, attribute){
+    if(attribute.options.readOnly !== true) {
+      this._super(snapshot, json, key, attribute);
+    }
+  }
+});
