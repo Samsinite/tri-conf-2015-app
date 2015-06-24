59c51edf1e7d1ca39b02ff634b33803b406866c5
Brendan Quinn <brendan@squidandcrow.com>
2015-06-24 09:51:09 -0700

    Update events per Samsinite's suggetions

---
+++ app/controllers/events.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	isAllEventsHidden : false,
+	isCultureEventsHidden : false,
+	isCommunityEventsHidden : false,
+	isDesignEventsHidden : false,
+	isEntrepreneurshipEventsHidden : false,
+	isTechnologyEventsHidden : false,
+	eventType : function() {
+		console.log(true);
+		return true;
+	},
+	actions : {
+		filterAll : function(){
+			this.set('isAllEventsHidden', !this.get('isAllEventsHidden'));
+		},
+		filterCulture : function(){
+			this.set('isCultureEventsHidden', !this.get('isCultureEventsHidden'));
+		},
+		filterCommunity : function(){
+			this.set('isCommunityEventsHidden', !this.get('isCommunityEventsHidden'));
+		},
+		filterDesign : function(){
+			this.set('isDesignEventsHidden', !this.get('isDesignEventsHidden'));
+		},
+		filterEntrepreneurship : function() {
+			this.set('isEntrepreneurshipEventsHidden', !this.get('isEntrepreneurshipEventsHidden'));
+		},
+		filterTechnology : function() {
+			this.set('isTechnologyEventsHidden', !this.get('isTechnologyEventsHidden'));
+		}
+	}
+});
--- app/routes/events.js
+++ app/routes/events.js
@@ -1,33 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	toggleEvents : function(buttonClass, eventClass) {
-		if(jQuery(buttonClass).hasClass('disabled')) {
-			jQuery(eventClass).parent().parent().show();
-			jQuery(buttonClass).removeClass('disabled');
-		} else {
-			jQuery(eventClass).parent().parent().hide();
-			jQuery(buttonClass).addClass('disabled');
-		}
-	},
 	model: function() {
 		return this.store.findAll('event');
-	},
-	actions : {
-		filterAll : function(){
-			this.toggleEvents('.pill-triconf', '.event');
-		},
-		filterCulture : function(){
-			this.toggleEvents('.pill-music', '.music');
-		},
-		filterCommunity : function(){
-			this.toggleEvents('.pill-activism', '.activism');
-		},
-		filterDesign : function(){
-			this.toggleEvents('.pill-art-and-design', '.art-and-design');
-		},
-		filterEntrepreneurship : function() {
-			this.toggleEvents('.pill-entrepreneurship', '.entrepreneurship');
-		}
 	}
 });
