1a44ab8aa5477ae98c30130c00d95ec51b6c327b
Ken Schmidt <kenschmidt@gmail.com>
2015-07-05 12:13:39 -0700

    adding complete-check component

---
+++ app/components/complete-check.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+import inject from 'ember-cli-injection/inject';
+
+var injectSession = inject('session');
+
+export default Ember.Component.extend({
+  session: injectSession('main'),
+  activity: null,
+  activityList: null,
+
+  completed: function () {
+    var didComplete = false;
+    var that = this;
+    if(this.get('activityList')) {
+      this.get('activityList').forEach(function(activity){
+        if(activity === that.get('activity')) {
+          didComplete = true;
+        }
+      });
+    }
+    return didComplete;
+  }.property('activityList', 'activity'),
+
+  actions: {
+    checkIn: function() {
+      this.sendAction('checkIn', this.get('activity'));
+    }
+  }
+});
