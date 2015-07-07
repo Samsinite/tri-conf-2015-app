1f42a31032b09131e9ef6cadb705e7012ab0dd8e
Ken Schmidt <kenschmidt@gmail.com>
2015-07-07 09:04:39 -0700

    removing some unused options to bootstrap-select-time that were causing errors

--- app/components/bootstrap-select-time.js
+++ app/components/bootstrap-select-time.js
@@ -1,6 +1,4 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['form-group', 'bootstrap-select-time-component'],
-  mindate: new Date(2015, 06, 14),
-  maxdate: new Date(2015, 06, 20),
 });
