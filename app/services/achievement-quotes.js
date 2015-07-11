dcfc652579e284cd9653e3d441e0c5ee801583d7
Squinnt <sara@squidandcrow.com>
2015-07-11 12:46:02 -0700

    Added (commented out) achievment notification messages

--- app/services/achievement-quotes.js
+++ app/services/achievement-quotes.js
@@ -1,4 +1,87 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+
+	/*
+
+	Urban Pedestrian challenge complete:
+	'"Every man must decide whether he will walk in the light of creative altruism or in the darkness of destructive selfishness." &#8212; Martin Luther King, Jr.'
+
+	Super Cycle challenge complete:
+	'"I can think. I can sleep. I can move. I can ride my bike. I can dream." &#8212; Bill Walton'
+
+	Another One Rides the Bus challenge complete:
+	'"I think the thing to do is enjoy the ride while you\'re on it." &#8212; Johnny Depp'
+
+	All three transportation modes used:
+	'You like variety in your travel! Thanks for using feet, bikes, and shuttles instead of driving!' Add image: public/images/badges/transportation_100x100.png
+
+	Event at Fuse attended:
+	'Visited Fuse! "Coworking is not about shared office space, it\’s about making a dynamo of serendipity, and that wheel can turn for anyone who just takes the step inside." &#8212; Erik Ralston'
+
+	Event at John Dam Plaza attended:
+	'Visited John Dam Plaza! "Nature surrounds us, from parks and backyards to streets and alleyways. Next time you go out for a walk, tread gently and remember that we are both inhabitants and stewards of nature in our neighbourhoods." &#8212; David Suzuki'
+
+	Event at Adventures Underground attended:
+	'Visited Adventures Underground! “Begin at the beginning," the King said, very gravely, "and go on till you come to the end: then stop.” &#8212; Lewis Carroll, ,em>Alice in Wonderland</em>'
+
+	Event at Uptown Theatre attended:
+	'Visited Uptown Theatre! "The theater is so endlessly fascinating because it\\'s so accidental. It's so much like life." &#8212; Arthur Miller'
+
+	Event at Richland Public Library:
+	'Visited Richland Public Library! "A library is the delivery room for the birth of ideas, a place where history comes to life." &#8212; Norman Cousins'
+
+	All events visited:
+	'You've been to every TriConf location! You do get around, don\'t you?' Add image: public/images/badges/location_100x100.png
+
+	One film event attended:
+	'"A film is never really good unless the camera is an eye in the head of a poet." &#8212; Orson Welles'
+
+	Two film events attended:
+	'"Filmmaking, like any other art, is a very profound means of human communication; beyond the professional pleasure of succeeding or the pain of failing, you do want your film to be seen, to communicate itself to other people." &#8212; Kenneth Lonergan'
+
+	Three film events attended: 
+	'Hello, indie film lover! Thanks for supporting local filmmakers!' Add image: public/images/badges/film_100x100.png
+
+	Music event on Friday attended:
+	'"Music is a moral law. It gives soul to the universe, wings to the mind, flight to the imagination, and charm and gaiety to life and to everything." &#8212; Plato'
+
+	Music event on Saturday attended:
+	'"Music is a higher revelation than all wisdom and philosophy." &#8212; Ludwig van Beethoven'
+
+	Music events on Friday and Saturday attended:
+	'Two days of local music?! That\'s my jam! ^_-' Add image: public/images/badges/music_100x100.png
+
+	Culture/Arts/Music talk attended:
+	'"Culture makes people understand each other better. And if they understand each other better in their soul, it is easier to overcome the economic and political barriers. But first they have to understand that their neighbour is, in the end, just like them, with the same problems, the same questions." &#8212; Paulo Coelho'
+
+	Community/Issues/Sustainability talk attended:
+	'"If we each take responsibility in shifting our own behavior, we can trigger the type of change that is necessary to achieve sustainability for our race or this planet. We change our planet, our environment, our humanity every day, every year, every decade, and every millennia." &#8212; Yehuda Berg'
+
+	Technology talk attended:
+	'"Technology gives us power, but it does not and cannot tell us how to use that power. Thanks to technology, we can instantly communicate across the world, but it still doesn\'t help us know what to say." &#8212; Jonathan Sacks'
+
+	Design/Communication talk attended:
+	'"Good design is making something intelligible and memorable. Great design is making something memorable and meaningful." &#8212; Dieter Rams'
+
+	Entrepreneurship talk attended:
+	'"We begin to change the world when we stimulate long-term prosperity using technology. There is not a problem that's large enough that innovation and entrepreneurship can\'t solve." &#8212; Naveen Jain'
+
+	At least one talk in each category attended:
+	'You have attended talks in every category! Many and varied are your interests.' Add image: public/images/badges/talks_100x100.png
+
+	One restaurant visited:
+	'"There is no sincerer love than the love of food." &#8212; George Bernard Shaw'
+
+	Two restaurants visited:
+	'"Food, in the end, in our own tradition, is something holy. It's not about nutrients and calories. It's about sharing. It's about honesty. It\'s about identity." &#8212; Louise Fresco'
+
+	Three restaurants visited:
+	'"Eating is not merely a material pleasure. Eating well gives a spectacular joy to life and contributes immensely to goodwill and happy companionship. It is of great importance to the morale." &#8212; Elsa Schiaparelli'
+
+	Four restaurants visited:
+	'Thanks for supporting local restaurants. We appreciate it, and I know they do, too!' Add image: public/images/badges/food_100x100.png
+
+	*/
+
 });
