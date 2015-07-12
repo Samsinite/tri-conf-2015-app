2211f6a573cb295c758883364ebbd132cc0718e5
Ken Schmidt <kenschmidt@gmail.com>
2015-07-12 16:37:54 -0700

    adding computed fields to the users for the leaderboard

--- app/controllers/leaderboard.js
+++ app/controllers/leaderboard.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  eventsLeaderboard: Ember.computed('model', function() {
+    return createSortableArray(this.get('model'), ['date'], true);
+  }),
 });
--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,24 @@ export default DS.Model.extend({
   bookmarkEvents: DS.hasMany('event', {async: true}),
   ateAt: DS.hasMany('restaurant', {async: true}),
   achieved: DS.hasMany('challenge', {async: true}),
-  points: DS.attr('number'),
   displayName: DS.attr('string'),
-  isAdmin: DS.attr('boolean', { readOnly: true })
+  isAdmin: DS.attr('boolean', { readOnly: true }),
+  numEvents() {
+    return this.get('attendedEvents').length;
+  }.property('attendedEvents'),
+  numBookmarks() {
+    return this.get('attendedEvents').length;
+  }.property('attendedEvents'),
+  numRestaurants() {
+    return this.get('ateAt').length;
+  }.property('ateAt'),
+  numAchieved() {
+    return this.get('achieved').length;
+  }.property('achieved'),
+  totalChecks() {
+    return this.get('attendedEvents').length +
+            this.get('bookmarkEvent').length +
+            this.get('ateAt').length +
+            this.get('achieved').length;
+  }.property('attendedEvents', 'bookmarkEvent', 'ateAt', 'achieved'),
 });
